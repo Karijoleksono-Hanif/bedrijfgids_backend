5e445bbbe5f2679bc7f77248e6232888627e7a64
Karijoleksono-Hanif <karijoleksono-suwondo@hotmail.com>
2022-12-30 02:39:29 -0300

    Applicatie update 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,8 +7,8 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
 //        DummyDataService.createDummyData();
-        //Applicatie.Menu();
+        Applicatie.Menu();
 //        DataService.deleteBedrijf();
-        DataService.deleteMediaFiles();
+//        DataService.deleteMediaFiles();
     }
 }
