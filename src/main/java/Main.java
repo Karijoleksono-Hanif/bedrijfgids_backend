8ac8ed13607d81390940c716a95867b542776a2d
Hanif Karijoleksono <karijoleksono-suwondo@hotmail.com>
2023-01-27 19:29:06 -0300

    Blok2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
         Applicatie.Menu();
-        //
+        //test
     }
 }
